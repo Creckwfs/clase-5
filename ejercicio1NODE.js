f9c2316348a0c3f6c68e60cfcb9444a0579f676b
alexdelamora <creckwfs@gmail.com>
2019-09-06 21:45:02 -0500

    nuevos archivos

---
+++ ejercicio1NODE.js
@@ -0,0 +1,5 @@
+const hambre = parseFloat("1")
+if (hambre === 1) {
+    console.log("Estas feliz")
+} else { 
+    console.log("Estas triste") }
---
+++ juego.js
@@ -0,0 +1,9 @@
+<script> 
+    const nivelPersonaje = parseFloat(prompt("Escribe tu nivel de personaje"))
+    if nivelPersonaje <= 9 {
+	alert("Eres Aprendiz") }
+    if nivelPersonaje <= 19 {
+	alert("Eres acrobata") }
+    if nivelPersonaje > 19 { 
+    alert("Soy un ninja")  }
+</script>
